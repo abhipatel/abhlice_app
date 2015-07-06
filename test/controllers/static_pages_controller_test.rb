4d95aa6647a217fd35268d9cbd3f30d1af3b2ade
Abhishek Patel <abhi@makeworldwide.com>
2015-07-06 20:21:48 +0100

    Add guard and basic static pages controller tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
+  test "should get logged out home" do
     get :home
     assert_response :success
+    assert_select "h1", "Alice & Abhi"
   end
 
+  test "should get logged in home" do
+    cookies[:has_access] = true
+    get :home
+    assert_response :success
+    assert_select "h3", "Venue"
+  end
 end
